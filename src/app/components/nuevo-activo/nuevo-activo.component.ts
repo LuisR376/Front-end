1dcac4256dc651b3d7fd90ec7aed98b62b2b806b
Becario Morales <luis_muscul30@hotmail.com>
2023-03-28 15:19:55 -0600

    nuevo-activo steps

--- src/app/components/nuevo-activo/nuevo-activo.component.ts
+++ src/app/components/nuevo-activo/nuevo-activo.component.ts
@@ -1,82 +1,69 @@
-import { Component,OnInit} from '@angular/core';
-import {MenuItem} from 'primeng/api';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 import { MessageService } from "primeng/api";
 import { authGuardService } from "src/app/service/auth-guard.service";
 import { CustomerService } from '../../service/CustomerService';
-import {Activos} from '../model/activos.model'
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Activos } from '../model/activos.model'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-nuevo-activo',
   templateUrl: './nuevo-activo.component.html',
   styleUrls: ['./nuevo-activo.component.css']
 })
-export class NuevoActivoComponent implements OnInit{
+export class NuevoActivoComponent implements OnInit {
   constructor(
-     private messageService: MessageService,
-     public _authGuardService: authGuardService,
-     public ticketService: CustomerService,
-     private fb: FormBuilder
-     ) {
-        
-     }
-     activoInformacion!: MenuItem[];
-     tablaActivos !: Activos;
-  
-  ngOnInit() {
-      this.activoInformacion =  [{
-              label: 'Tipo de Activo',
-              routerLink: 'Paso1'
-          },
-          {
-              label: 'Datos del cliente',
-              routerLink: 'seat'
-          },
-          {
-              label: 'Ubicacion',
-              routerLink: 'payment'
-          },
-          {
-              label: 'Datos del Equipo',
-              routerLink: 'confirmation'
-          },
-          {
-            label: 'Accesorio',
-            routerLink: ''
-        }
-      ];
-      /*
-      this.tablaActivos = this.ticketService.paymentComplete$.subscribe((personalInformation) =>{
-        this.messageService.add({severity:'success', summary:'Order submitted', detail: 'Dear, ' + personalInformation.firstname + ' ' + personalInformation.lastname + ' your order completed.'});
-    });
-}
+    private messageService: MessageService,
+    public _authGuardService: authGuardService,
+    public ticketService: CustomerService,
+    private fb: FormBuilder
+  ) {
 
-ngOnDestroy() {
-    if (this.tablaActivos) {
-        this.tablaActivos.unsubscribe();
-    }
-    */
-}
-recoInfo: FormGroup = this.fb.group({
-    id:[''], 
-    idactivos:[''],
-    idlugar:[''],
-    idarea:[''],
-    nombre_propietario:[''],
-    nombre_equipo:[''],
-    num_empleado:[''],
-    password:[''],
-    fecha_mantenimien:[''],
-    valor_monetario:[''],
-    estado:[''],
-    descripcion:[''],
-    tipo_de_conexion:[''],
-    iddetallepc:[''],
-    idLicencias:[''],
-    idtipoactivo:['2'],
-    host_teamviewer:[''],
-    password_teamviewer:[''],
-    calculoEstimado:[''],
-    Pertenencia:['']
-    
-  });
+  }
+  activoInformacion!: MenuItem[];
+  tablaActivos !: Activos;
+  items = [
+    { label: 'Tipo de Activo' },
+    { label: 'Datos del cliente' },
+    { label: 'Ubicacion' },
+    { label: 'Datos del Equipo' },
+    { label: 'Accesorio' }
+  ];
+  activeIndex = 0;
+  step1Form!: FormGroup;
+  step2Form!: FormGroup;
+  step3Form!: FormGroup;
+  step4Form!: FormGroup;
+  step5Form!: FormGroup;
+
+
+  ngOnInit(): void {
+    this.step1Form = this.fb.group({
+      pertenencias: ['', [Validators.required]],
+      idtipoactivo: ['', [Validators.required, Validators.email]]
+    });
+
+    this.step2Form = this.fb.group({
+      nombre_propietario: ['', [Validators.required]],
+      num_empleado: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+
+    this.step3Form = this.fb.group({
+      idlugar: ['', [Validators.required]],
+      idarea: ['', [Validators.required]]
+    });
+
+    this.step4Form = this.fb.group({
+      nombre_equipo: ['', [Validators.required]],
+      valor_monetario: ['', [Validators.required]]
+    });
+
+    this.step5Form = this.fb.group({
+      descripcion: ['', [Validators.required]]
+    });
+  }
+  onActiveIndexChange(event: any) {
+
+  }
 }
